4f1ab50237e2931e31fcc47781bdb32010136a71
corentin1706 <corentin.latimier@utbm.fr>
2020-10-21 16:59:55 +0200

    update

--- inscription.php
+++ inscription.php
@@ -11,45 +11,45 @@
 
 <form method="post" action="formulaire.php">
         <label>
-            Nom : <input type="text" name="nom" required="Champ obligatoire"/>
+            Nom : <input type="text" name="nom" required/>
         </label>
-    </br>
+    <br>
         <label>
-            Prénom : <input type="text" name="prenom" required="Champ obligatoire"/>
+            Prénom : <input type="text" name="prenom" required/>
         </label>
-    </br>
+    <br>
          <label>
-            Email : <input type="text" name="email" required="Champ obligatoire"/>
+            Email : <input type="text" name="email" required/>
         </label>
-    </br>
+    <br>
          <label>
-            Adresse : <input type="text" name="adresse_actuelle" required="Champ obligatoire" />
+            Adresse : <input type="text" name="adresse_actuelle" required />
         </label>
-    </br>
+    <br>
         <label>
-            Ville de residence : <input type="text" name="ville_de_residence" required="Champ obligatoire" />
+            Ville de residence : <input type="text" name="ville_de_residence" required />
         </label>
-   </br>
+   <br>
         <label>
             Date de naissance: <input type="date" name="date_naissance" value="date"  min="2018-01-01" max="2018-12-31">
         </label>
-    </br>
+    <br>
         <label>
-            Mot de passe: <input type="password" name="password" required="Champ obligatoire">
+            Mot de passe: <input type="password" name="password" required>
         </label>
-    </br>
+    <br>
         <label>
             Lien linkedin: <input type="text" name="lien_linkedin" >
         </label>
-    </br>
+    <br>
         <label>
             Biographie: <TEXTAREA name="biographie" rows=10 COLS=40></TEXTAREA>
-        <label>
-    </br>
+        </label>
+    <br>
         <label>
             Numéro de Téléphone: <input type="tel"  name="num_telephone" pattern="[0-9]{10}" required>
         </label>
-    </br>
+    <br>
         <label>
             Photo de profil: <input type="file" name="photo_profil" accept="image/jpeg, image/png">
          </label>
