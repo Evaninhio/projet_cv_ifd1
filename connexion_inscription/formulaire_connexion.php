eaec55babb4bdb0e0df0202dddce63aaba78add6
corentin1706 <corentin.latimier@utbm.fr>
2020-10-22 21:19:07 +0200

    update

--- connexion_inscription/formulaire_connexion.php
+++ connexion_inscription/formulaire_connexion.php
@@ -3,36 +3,34 @@ $email=$_POST["email"];
 $password=$_POST["password"];
 
 $bdd=new PDO("mysql:host=localhost;dbname=cv_generator;charset=utf8", "root", "");
-$req_test=$bdd->prepare("SELECT COUNT(*) from utilisateur where email='$email';");
+$req_test=$bdd->prepare("SELECT * from utilisateur where email='$email';");
 $req_test->execute();
 $data=$req_test->fetch();
 
-echo $data[0];
 
-if($data[0]==0)
+
+if($data)
 {
-    header("Location: connexion.php");
-    exit();
-}
-else{
     $bdd=new PDO("mysql:host=localhost;dbname=cv_generator;charset=utf8", "root", "");
-    $req=$bdd->prepare("SELECT COUNT(*) from utilisateur where email='$email' AND password=PASSWORD('$password');");
+    $req=$bdd->prepare("SELECT * from utilisateur where email='$email' AND password=PASSWORD('$password');");
     $data=$req->fetch();
 
-
-//
-//    if($data==1)
-//    {
-//        echo "test";
-//    }
-//    else
-//    {
-//        echo"error";
-//        header("Location: connexion.php");
-//        exit();
-//    }
+    if($data)
+    {
+        echo"error";
+        header("Location: connexion.php");
+        exit();
+    }
+    else
+    {
+        echo "test";
+    }
 
 }
-
+else{
+    header("Location: connexion.php");
+    //no email corresponding
+    exit();
+}
 
 ?>
