382b1d7d94b90031e4e694ece65b4445c94634b3
corentin1706 <corentin.latimier@utbm.fr>
2020-10-30 16:11:56 +0100

    update

--- generator_cv/experience_benevolat.php
+++ generator_cv/experience_benevolat.php
@@ -68,6 +68,8 @@ session_start();
 
     <a href="competences.php"><i class="fas fa-check"></i><span>Compétences</span></a>
 
+    <a href="#"><i class="fas fa-book"></i><span>Qualifications</span></a>
+
     <a href="langues.php"><i class="fas fa-language"></i><span>Langues</span></a>
 
     <a href="passions.php"><i class="fas fa-heart"></i><span>Passions</span></a>
