f83e8ac913bf194e2834fa6c8b551fe5189951ac
corentin1706 <corentin.latimier@utbm.fr>
2020-10-31 11:22:42 +0100

    commentaire du code + update nom des images +update organisation du code

--- connexion_inscription/gestion_formulaire/formulaire_connexion.php
+++ connexion_inscription/gestion_formulaire/formulaire_connexion.php
@@ -1,34 +1,36 @@
 <?php
 
-
-$email=$_POST["email"];
-$password=$_POST["password"];
-
-$bdd=new PDO("mysql:host=localhost;dbname=cv_generator;charset=utf8", "root", "");
-$req_test=$bdd->prepare("SELECT COUNT(*) from utilisateur where email=? AND password=PASSWORD(?);");
-$req_test->execute([$email,$password]);
-$data=$req_test->fetch();
-
-if($data['COUNT(*)']==0)
-{
-    header("Location: ../connexion.php?error=1");
-    exit();
-}
-else{
+    $email=$_POST["email"];
+    $password=$_POST["password"];
+    //on récupère les variables du formulaire
 
     $bdd=new PDO("mysql:host=localhost;dbname=cv_generator;charset=utf8", "root", "");
-    $req=$bdd->prepare("SELECT prenom,nom,photo_profil from utilisateur where email=?;");
-    $req->execute([$email]);
-    $data=$req->fetch();
-    session_start();
+    $req_test=$bdd->prepare("SELECT COUNT(*) from utilisateur where email=? AND password=PASSWORD(?);");
+    $req_test->execute([$email,$password]);
+    $data=$req_test->fetch();
 
-    $_SESSION["prenom"]=$data["prenom"];
-    $_SESSION["nom"]=$data["nom"];
-    $_SESSION["photo_profil"]=$data["photo_profil"];
-    $_SESSION["email"]=$email;
+    //on compte le nombre de comptes qui correspondent
+
+    if($data['COUNT(*)']==0)
+    {
+        //aucun compte ne correspond on renvoie une erreur dans l'url sur la page de connexion
+        header("Location: ../connexion.php?error=1");
+    }
+    else{
+
+        $bdd=new PDO("mysql:host=localhost;dbname=cv_generator;charset=utf8", "root", "");
+        $req=$bdd->prepare("SELECT prenom,nom,photo_profil from utilisateur where email=?;");
+        $req->execute([$email]);
+        $data=$req->fetch();
+        session_start();
+        //si un compte correspond on récupère dans des variables de sesson le prenom,le nom,l'email et la photo de profil de l'utilisateur
+        $_SESSION["prenom"]=$data["prenom"];
+        $_SESSION["nom"]=$data["nom"];
+        $_SESSION["photo_profil"]=$data["photo_profil"];
+        $_SESSION["email"]=$email;
 
 
-       header("Location: ../../home/home.php?connected");
-}
-
+        header("Location: ../../home/home.php?connected");
+        //on renvoie sur la page d'accueil avec la variable connected dans l'url
+    }
 ?>
