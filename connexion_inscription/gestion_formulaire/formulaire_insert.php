ed3ad033e7682240c2e21cfb63ffaf60f5a2fbb1
corentin1706 <corentin.latimier@utbm.fr>
2020-10-31 11:31:26 +0100

    commentaire du code + update nom des images +update organisation du code

--- connexion_inscription/gestion_formulaire/formulaire_insert.php
+++ connexion_inscription/gestion_formulaire/formulaire_insert.php
@@ -1,42 +1,39 @@
 <?php
+    $nom=strtoupper($_POST["nom"]);
+    $prenom=strtoupper($_POST["prenom"]);
+    $email=$_POST["email"];
+    $adresse_actuelle=strtoupper($_POST["adresse_actuelle"]);
+    $ville_de_residence=strtoupper($_POST["ville_de_residence"]);
+    $date_naissance=$_POST["date_naissance"];
+    $password=$_POST["password"];
+    $lien_linkedin=$_POST["lien_linkedin"];
+    $biographie=$_POST["biographie"];
+    $num_telephone=$_POST["num_telephone"];
+    $photo_profil=$_POST["photo_profil"];
 
+    //on récupère les variables du formulaire à insérer dans la base de données
+    //la fonction strtoupper permet de passer certaines variables en majuscules
 
-$nom=strtoupper($_POST["nom"]);
-$prenom=strtoupper($_POST["prenom"]);
-$email=$_POST["email"];
-$adresse_actuelle=strtoupper($_POST["adresse_actuelle"]);
-$ville_de_residence=strtoupper($_POST["ville_de_residence"]);
-$date_naissance=$_POST["date_naissance"];
-$password=$_POST["password"];
-$lien_linkedin=$_POST["lien_linkedin"];
-$biographie=$_POST["biographie"];
-$num_telephone=$_POST["num_telephone"];
-$photo_profil=$_POST["photo_profil"];
+    $bdd=new PDO("mysql:host=localhost;dbname=cv_generator;charset=utf8", "root", "");
+    $req_test=$bdd->prepare("SELECT COUNT(*) from utilisateur where email=?;");
+        //on regarde si il existe déjà un compte avec la meme adresse email
+    $req_test->execute([$email]);
+    $data=$req_test->fetch();
 
-$bdd=new PDO("mysql:host=localhost;dbname=cv_generator;charset=utf8", "root", "");
-$req_test=$bdd->prepare("SELECT COUNT(*) from utilisateur where email='$email';");
-$req_test->execute();
-$data=$req_test->fetch();
+    if($data[0]!=0)
+    {
+        //si il en existe déja on renvoie sur la pag d'inscription avec un message d'erreur
+        header("Location: ../inscription.php?error=1");
+        exit();
+    }
+    else
+    {
+        //sinon on insère les valeurs dans la table utilisateur pour créer une nouvelle entrée
+        $bdd= new PDO("mysql:host=localhost;dbname=cv_generator;charset=utf8", "root", "");
+        $req=$bdd->prepare("INSERT INTO utilisateur VALUES (?,?,?,?,?,?,?,PASSWORD(?),?,?,?);");
+        $req->execute([$email,$nom,$prenom,$adresse_actuelle,$ville_de_residence,$lien_linkedin,$date_naissance,$password,$biographie,$num_telephone,$photo_profil]);
 
-if($data[0]!=0)
-{
-    header("Location: ../inscription.php?error=1");
-    exit();
-}
-else
-{
+        header("Location: ../../home/home.php?creation_compte");
+        //on renvoie sur la page d'accueil en affichant dans l'url que le compte a été crée avec succès
 
-    $bdd= new PDO("mysql:host=localhost;dbname=cv_generator;charset=utf8", "root", "");
-    $req=$bdd->prepare("INSERT INTO utilisateur VALUES ('$email','$nom','$prenom','$adresse_actuelle','$ville_de_residence','$lien_linkedin','$date_naissance',PASSWORD('$password'),'$biographie','$num_telephone','$photo_profil');");
-    $req->execute();
-
-    session_start();
-    $_SESSION["prenom"]=$data["prenom"];
-    $_SESSION["nom"]=$data["nom"];
-    $_SESSION["photo_profil"]=$data["photo_profil"];
-    header("Location: ../../home/home.php?creation_compte");
-
-
-}
-
-?>
+    }
