3f234bbd7b63b63e2de188f462e5e455e21bbd59
corentin1706 <corentin.latimier@utbm.fr>
2020-10-31 10:55:50 +0100

    commentaire du code + update nom des images

--- connexion_inscription/inscription.php
+++ connexion_inscription/inscription.php
@@ -1,121 +1,126 @@
 <!DOCTYPE html>
 <html lang="fr">
 
-<head>
-    <meta charset="UTF-8">
-    <title>My Online CV- INSCRIPTION </title>
-    <link rel="stylesheet" href="inscription_css.css">
-    <link rel="stylesheet" href="../bootstrap-4.5.3-dist/css/bootstrap.css">
-    </head>
+    <head>
+        <meta charset="UTF-8">
+        <title>My Online CV- INSCRIPTION </title>
+        <link rel="stylesheet" href="inscription_css.css">
+        <link rel="stylesheet" href="../bootstrap-4.5.3-dist/css/bootstrap.css">
+        </head>
 
 
-<body class="text_center">
+    <body class="text_center">
 
-<slider>
-    <slide></slide>
-    <slide></slide>
-    <slide></slide>
-    <slide></slide>
-</slider>
-
-
-
-<form class="form-signin" method="post" action="formulaire_insert.php">
+    <slider>
+        <!--slider de modèles de cv-->
+        <slide></slide>
+        <slide></slide>
+        <slide></slide>
+        <slide></slide>
+    </slider>
 
 
-    <h1 class="mb-3" id="title">Inscrivez vous dès aujourd'hui</h1>
 
-    <div class="error">
-        <?php
-        if (isset($_GET['error']))
-        { echo "<p>Adresse email déja utilisée</p>";
-        }
-        ?>
-    </div>
+    <form class="form-signin" method="post" action="formulaire_insert.php">
+        <!--formulaire d'inscription -->
 
 
-    <!--formulaire inscription nouvel utilisateur-->
-    <ul>
-        <li>
-            <label>
-                Nom : <input type="text" name="nom" required/>
-            </label>
-            <br>
-        </li>
-        <li>
-            <label>
-                Prénom : <input type="text" name="prenom" required/>
-            </label>
-            <br>
-        </li>
+        <h1 class="mb-3" id="title">Inscrivez vous dès aujourd'hui</h1>
 
-        <li>
-            <label>
-                Email : <input type="email" name="email" required placeholder="xxxxxxx@xxxxx.com"/>
-            </label>
-            <br>
-        </li>
-
-        <li>
-            <label>
-                Adresse : <input type="text" name="adresse_actuelle" required  />
-            </label>
-            <br>
-        </li>
-
-        <li>
-            <label>
-                Ville de residence : <input type="text" name="ville_de_residence" required />
-            </label>
-            <br>
-        </li>
-
-        <li>
-            <label>
-                Date de naissance: <input type="date" name="date_naissance" value="<?php echo $today = date("Y-m-d")?>" max= "<?php echo $today = date("Y-m-d")?>"  required />
-            </label>
-            <br>
-        </li>
-
-        <li>
-            <label>
-                Mot de passe: <input type="password" name="password" min="8" required>
-            </label>
-            <br>
-        </li>
-            <label>
-                Lien linkedin: <input type="text" name="lien_linkedin">
-            </label>
-            <br>
-        <li/>
+        <div class="error">
+            <!--si l'adresse mail est déja présente dans la base de données on renvoie error dans l'url-->
+            <!--on teste si error est set dans l'url avec la méthode get-->
+            <!--si c'est le cas on affiche un message d'erreur-->
+            <?php
+                if (isset($_GET['error'])){ echo "<p>Adresse email déja utilisée</p>";}
+            ?>
+        </div>
 
 
-        <li>
-            <label>
-                Biographie: <TEXTAREA name="biographie" maxlength="200"></TEXTAREA>
-            </label>
-            <br>
-        </li>
+        <ul>
+            <li>
+                <label>
+                    Nom : <input type="text" name="nom" required/>
+                </label>
+                <br>
+            </li>
+            <li>
+                <label>
+                    Prénom : <input type="text" name="prenom" required/>
+                </label>
+                <br>
+            </li>
 
-        <li>
-            <label>
-                Numéro de Téléphone: <input type="number"  name="num_telephone" pattern="[0-9]{10}">
-            </label>
-            <br>
-        </li>
+            <li>
+                <label>
+                    Email : <input type="email" name="email" required placeholder="xxxxxxx@xxxxx.com"/>
+                    <!--placeholder permet d'afficher un exemple-->
+                </label>
+                <br>
+            </li>
 
-        <li>
-            <label>
-                Photo de profil: <input type="file" name="photo_profil" accept="image/png" required>
-            </label>
-        </li>
-    </ul>
+            <li>
+                <label>
+                    Adresse : <input type="text" name="adresse_actuelle" required  />
+                </label>
+                <br>
+            </li>
 
-    <input type="submit" id="button_creation" value=" Créer un compte! "/>
-</form>
+            <li>
+                <label>
+                    Ville de residence : <input type="text" name="ville_de_residence" required />
+                </label>
+                <br>
+            </li>
+
+            <li>
+                <label>
+                    Date de naissance: <input type="date" name="date_naissance" value="<?php echo $today = date("Y-m-d")?>" max= "<?php echo $today = date("Y-m-d")?>"  required />
+                    <!--on affiche par défaut la date du jour et on bloque la date de naissance minimum à la date du jour-->
+                </label>
+                <br>
+            </li>
+
+            <li>
+                <label>
+                    Mot de passe: <input type="password" name="password" min="8" required>
+                </label>
+                <br>
+            </li>
+                <label>
+                    Lien linkedin: <input type="text" name="lien_linkedin">
+                </label>
+                <br>
+            <li/>
 
 
-</body>
+            <li>
+                <label>
+                    Biographie: <TEXTAREA name="biographie" maxlength="200"></TEXTAREA>
+                </label>
+                <br>
+            </li>
+
+            <li>
+                <label>
+                    Numéro de Téléphone: <input type="number"  name="num_telephone" pattern="[0-9]{10}">
+                    <!--l'attribut pattern permet de vérifier si lle num de téléphone contient bien 10 chiffres allant de 0 à 9-->
+                </label>
+                <br>
+            </li>
+
+            <li>
+                <label>
+                    Photo de profil: <input type="file" name="photo_profil" accept="image/png" required>
+                </label>
+            </li>
+        </ul>
+
+        <input type="submit" id="button_creation" value=" Créer un compte! "/>
+    </form>
+
+
+    </body>
 
 </html
 
